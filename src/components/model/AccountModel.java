73f2b787e74a099d5ae825e4f7ec7de1930b1da6
MitsuyoRai-PC\MitsuyoRai <mr.dailc@gmail.com>
2018-03-04 17:28:22 +0700

    set hibernate-configuration

---
+++ src/components/model/AccountModel.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package components.model;
+
+import components.dto.AccountDTO;
+import java.util.List;
+
+/**
+ *
+ * @author MitsuyoRai
+ */
+public class AccountModel {
+    public String QUERY_CHECK_LOGIN = "from AccountDTO a where Usename='%s' and Password='%s'";
+    
+    public List<AccountDTO> checkLogin(String _username, String _password)
+    {
+        return QueryDB.GetInstance().executeHQLQuery(String.format(QUERY_CHECK_LOGIN, _username, _password));
+    }
+}
---
+++ src/components/model/QueryDB.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package components.model;
+
+import components.util.HibernateUtil;
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author MitsuyoRai
+ */
+public class QueryDB {
+    private static QueryDB _instance = null;
+    public static QueryDB GetInstance() {
+        if (_instance == null){
+            _instance = new QueryDB();
+        }
+        return _instance;
+    }
+    
+    public Boolean saveOrUpdate(Object obj){
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.saveOrUpdate(obj);
+            session.getTransaction().commit();
+            return true;
+        } catch (HibernateException he) {
+            return false;
+        }
+    }
+    
+    public Boolean delete(Object obj){
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.delete(obj);
+            session.getTransaction().commit();
+            return true;
+        } catch (HibernateException he) {
+            return false;
+        }
+    }
+
+    public List executeHQLQuery(String hql) {
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            Query q = session.createQuery(hql);
+            List resultList = q.list();
+            session.getTransaction().commit();
+            return resultList;
+        } catch (HibernateException he) {
+            return null;
+        }
+    }
+}
