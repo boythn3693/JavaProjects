d25217e4a5dcabc0becd8c6de63d6ad5ad3fb782
DICENTRAL\khoinguyen <vankhoicv@gmail.com>
2018-03-30 17:01:27 +0700

    add report

---
+++ sqlQuery/PrtDeliveryBillQuery.sql
@@ -0,0 +1,11 @@
+SELECT
+     CASE WHEN r."Status"=1THEN 'Thanh Cong'WHEN r."Status"=2THEN 'Da Huy'ELSE 'UNKWON'END AS "Trang_Thai",
+     r."deliverybillId" AS Ma_Phieu,
+     r."Datetime" AS Ngay,
+     p."PartnerName" AS Doi_Tac,
+     rd."Quantity" AS So_Luong,
+     pro."ProductName" AS San_Pham
+FROM
+     "deliverybill" r INNER JOIN "partner" p ON r."PartnerId" = p."PartnerId"
+     INNER JOIN "deliverybilldetail" rd ON r."deliverybillId" = rd."deliverybillId"
+     INNER JOIN "product" pro ON rd."ProductId" = pro."ProductId"
---
+++ sqlQuery/PrtReceiptQuery.sql
@@ -0,0 +1,11 @@
+SELECT
+     CASE WHEN r."Status"=1THEN 'Thanh Cong'WHEN r."Status"=2THEN 'Da Huy'ELSE 'UNKWON'END AS "Trang_Thai",
+     r."ReceiptId" AS Ma_Phieu,
+     r."Datetime" AS Ngay,
+     p."PartnerName" AS Doi_Tac,
+     rd."Quantity" AS So_Luong,
+     pro."ProductName" AS San_Pham
+FROM
+     "receipt" r INNER JOIN "partner" p ON r."PartnerId" = p."PartnerId"
+     INNER JOIN "receiptdetail" rd ON r."ReceiptId" = rd."ReceiptId"
+     INNER JOIN "product" pro ON rd."ProductId" = pro."ProductId"
